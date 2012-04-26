cc6fb98b00e3d44aea1701ee28d007a55eef8417
Steven Githens <swgithen@mtu.edu>
2012-04-26 16:47:24 -0400

    Adding dysplasia form and refactored via form

---
+++ via-js.js
@@ -0,0 +1,7 @@
+//alert("VIA JS Test 6");
+var vialmp = $j("#via-lmp");
+$j(document).ready(function() {
+    // $j("#via-lmp input").click(function(e) {
+    //     alert(getField("via-lmp.date"));
+    // });
+});
