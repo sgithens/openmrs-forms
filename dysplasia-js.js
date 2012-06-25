79c1d785159f9143bda326ee7a24d84bc4bfee40
Steven Githens <swgithen@mtu.edu>
2012-06-25 17:13:58 -0400

    updates for mut ex buttons

---
+++ dysplasia-js.js
@@ -0,0 +1,7 @@
+$j(window).load(function() {
+    openmrs.htmlformentry.initDetachedObsDatetime("combined7381datetime",["pos7381","neg7381"]);
+    openmrs.htmlformentry.initDetachedObsDatetime("combined7423datetime",
+                                                  ["normal7423","ascus7423","agus7423","lsil7423","hsil7423","squamous7423","adeno7423"]);
+    openmrs.htmlformentry.initDetachedObsDatetime("combined7426datetime",
+                                                  ["normal7426","cin17426","cin27426","cin37426","carcinoma7426"]);
+});
--- via-js.js
+++ via-js.js
@@ -1,5 +1,3 @@
 $j(window).load(function() {
-    alert("using load8");
     openmrs.htmlformentry.initDetachedObsDatetime("combinedDate",["pos7381","neg7381"]);
-    alert("using load9");
 });
